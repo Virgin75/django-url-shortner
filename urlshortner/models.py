0d7402da140fa4eec75e469f2565766b032be308
Virgin Bitton <virgin.bitton@gmail.com>
2021-12-17 21:47:46 +0100

    creation du modele principale de l'app UrlShortner

--- urlshortner/models.py
+++ urlshortner/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 # Create your models here.
+class Link(models.Model):
+    owner = models.ForeignKey(User , on_delete=models.CASCADE)
+    long_url = models.URLField(max_length=250)
+    short_slug = models.SlugField(max_length=50)
+    nb_hits = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.long_url}, short is: {self.short_slug}"
