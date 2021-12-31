8b28d50ce7a19b71cc1268f41e5955b5d8f0c567
Virgin Bitton <virgin.bitton@gmail.com>
2021-12-31 16:43:11 +0100

    mise en place du premier test

--- urlshortner/tests.py
+++ urlshortner/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+
+#@pytest.mark.django_db
+@pytest.mark.urls('config.urls')
+def test_home_is_displayed(client):
+    page_content = client.get('/shorten').content
+    assert b'Fill up the form to shorten your URL' in page_content
