7415f20b8732545ef892d0857d1b82742f9da552
Virgin Bitton <virgin.bitton@gmail.com>
2022-01-03 23:19:21 +0100

    ajout des tests sur l'app users

--- users/tests.py
+++ users/tests.py
@@ -1,4 +1,26 @@
+import pytest
 from django.contrib.auth import get_user_model
-from django.test import TestCase
 
+# Test login with wrong credentials
+@pytest.mark.django_db
+def test_links_list(client):
+    get_user_model().objects.create_user(
+        email='virgin@gmail.com', 
+        password='Azerty123$'
+    )
 
+    res = client.login(email='virgin@gmail.com', password='Lolol')
+
+    assert res is False
+
+# test login with right credentials
+@pytest.mark.django_db
+def test_links_list(client):
+    get_user_model().objects.create_user(
+        email='virgin@gmail.com', 
+        password='Azerty123$'
+    )
+
+    res = client.login(email='virgin@gmail.com', password='Azerty123$')
+
+    assert res is True
